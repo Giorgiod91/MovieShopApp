976234e07a90f8a57414a415618417cd1bb5f78b
Giorgio . D <109972616+Giorgiod91@users.noreply.github.com>
2024-04-21 19:27:02 +0200

    Update PricingCards.tsx
    added stripe payment

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -103,20 +103,18 @@ function PricingCards({redirect} : {redirect: boolean}) {
                     </ul>
                   
                 </div>
-                {redirect ? (
-                    <button>
-                    
-  <Link href="/register" className="mt-8 black rounded bg-in px-3.5 py-2 text-center text-sm font-semibold
+                 {redirect ? (
+              <a
+                href="https://buy.stripe.com/test_00g9CBd3O3KneoU144"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-8 black rounded bg-in px-3.5 py-2 text-center text-sm font-semibold
   leading-6 text-white sadow-sn hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
-  focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
-    
-      Get Started Now
-    
-  </Link>
-  </button>
-    
-
-) : (
+  focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
+              >
+                Get Started Now
+              </a>
+            ) : (
   plan.id && <CheckoutButton />
 )}
                 
